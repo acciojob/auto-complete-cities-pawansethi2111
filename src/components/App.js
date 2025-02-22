812cf251fb9c7e5e749d23edef3143f561c43637
Pawan <pawansethi2111@gmail.com>
2025-02-22 17:58:45 +0530

    uploaded

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,46 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
+import citiesArr  from "./citiesData.js"
+
+
+
 
 const App = () => {
+
+  const [sub, setSub] = useState("")
+  const [fileredCities, setFileredCities] = useState("")
+
+
+  function updateSub(e) {
+    let value = e.target.value
+    setSub(value)
+   let arr =  citiesArr.filter(city => city.toLowerCase().includes(value.toLowerCase()))
+   setFileredCities(arr)
+  }
+ 
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="main">
+      <form>
+         <input type="text"  placeholder="enter someting ... "
+           onChange={updateSub}
+           value={sub}
+         
+         />
+      </form>
+
+      <ul className="suggestions">
+        {
+         
+         fileredCities && fileredCities.length>0 &&  fileredCities.map((city, index) => {
+                return (
+                    <li key={index}>
+                        {city}  
+                    </li>
+                )
+            })
+        }
+      </ul>
     </div>
   )
 }
